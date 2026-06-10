f7a4f79dec5b6899832bb95b4a2e008a59718cda
Aryan Sharma <122178949+aryansh05@users.noreply.github.com>
2026-06-10 17:07:02 +0530

    Setup React Frontend with VITE and Typescript
    - Add home page for the frontend
    - Add design for login and signup page for the frontend
    
    Signed-off-by: Aryan Sharma <122178949+aryansh05@users.noreply.github.com>

---
+++ frontend/src/logic/Toggle.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function Toggle() {
+  const [isDark, setIsDark] = useState<boolean>(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") === "dark";
+    }
+    return false;
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (isDark) {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDark]);
+
+  const toggleTheme = () => setIsDark((prev) => !prev);
+
+  return { isDark, toggleTheme };
+}
+
+
+export default Toggle;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,23 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {BrowserRouter, Route, Routes} from "react-router";
+import App from "./App";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import RootLayout from "./pages/RootLayout";
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+)
